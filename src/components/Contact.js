e65e634d7dbeb04ec8893471e6fad2dc524158d5
BLR <work@billimarie.com>
2019-11-06 11:14:14 -0800

    Pushing up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Contact = () => (
-  <section className="bg-gray-200 p-6">
+  <section className="p-6">
     <h2 className="text-2xl font-light text-gray-900 leading-none mb-6">Contact</h2>
     <p>Contact information can go here.</p>
   </section>
